5eb1b9392e0493c0e521dc499cc6d73c37962b96
JosephJ12 <josephjung12@gmail.com>
2026-05-05 12:48:09 -0700

    Update user.go

--- appsec-projects/Securing-crAPI-AppSec-Risk-Assessment-and-Remediation/remediations/CR03/user.go
+++ appsec-projects/Securing-crAPI-AppSec-Risk-Assessment-and-Remediation/remediations/CR03/user.go
@@ -1,1 +1,15 @@
 
+```go
+// Post Field
+// New Post model using PostAuthor object
+// removes email and vehicleID fields from previous Author object
+type Post struct {
+	ID        string     `gorm:"primary_key;auto_increment" json:"id"`
+	Title     string     `gorm:"size:255;not null;unique" json:"title"`
+	Content   string     `gorm:"size:255;not null;" json:"content"`
+	Author    PostAuthor     `bson:"author" json:"author"`
+	Comments  []Comments `json:"comments"`
+	AuthorID  uint64     `sql:"type:int REFERENCES users(id)" json:"authorid"`
+	CreatedAt time.Time
+}
+```
